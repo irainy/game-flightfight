ddbe4d62b38d12eebab3271452c52fb901aa3fb5
irainy <sniky.hack@gmail.com>
2014-07-09 11:04:11 +0800

    init pomelo client

---
+++ pomelo/Pomelo.lua
@@ -0,0 +1,8 @@
+require("Protocol")
+
+
+local Pomelo = {}
+
+Pomelo.protocol = Protocol.version
+
+return Pomelo
---
+++ pomelo/Protocol.lua
@@ -0,0 +1,6 @@
+local Protocol = {}
+
+Protocol.name = "Cocos2dX-lua-websocket-v0.0.1"
+
+
+return Protocol
